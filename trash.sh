5e38a9a7495c113511217693e3e395814c4c7d5c
saihon <devqrx+saihon-github@gmail.com>
2023-10-25 08:15:29 +0900

    Improve code

--- trash.sh
+++ trash.sh
@@ -22,138 +22,149 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-func-usage() {
-	cat << HELP
+# set -eux
 
-Usage: $NAME [options] [files or directories]
+NAME=$(basename "$0")
+readonly NAME
 
-Options:
-  -a, --all         Show all items in Trash including hidden.
-  -e, --empty       Permanently delete items in Trash.
-  -c, --confirm     Confirm before delete items in Trash.
-  -l, --list        Show items in the Trash in long format.
-  -h, --help        Display this help and exit.
-  -v, --version     Output version information and exit.
+readonly VERSION="v0.0.3"
 
-HELP
-	exit 2
-}
-
-func-version() {
-	echo "$NAME: $VERSION"
-	exit 2
-}
+readonly TRASH_ROOT="/home/$USER/.local/share/Trash"
+readonly TRASH_FILES="$TRASH_ROOT/files"
 
 func-error() {
-	echo "Error: $1." 1>&2
-	exit 1
+    echo "Error: $1." 1>&2
+    exit 1
 }
 
 func-validation() {
-	if [[ "$1" =~ ^-([^-]+|$) ]] && [[ "$1" =~ ^-(.*[^$PATTERN_SHORT]+.*|)$ ]]; then
-		func-error "$1 is invalid as option"
-	fi
-	if [[ "$1" =~ ^-{2,} ]] && [[ ! "$1" =~ ^-{2}($PATTERN_LONG)$ ]]; then
-		func-error "$1 is invalid as option"
-	fi
+    if [[ "$1" =~ ^-([^-]+|$) ]] && [[ "$1" =~ ^-(.*[^$PATTERN_SHORT]+.*|)$ ]]; then
+        func-error "$1 is invalid as option"
+    fi
+    if [[ "$1" =~ ^-{2,} ]] && [[ ! "$1" =~ ^-{2}($PATTERN_LONG)$ ]]; then
+        func-error "$1 is invalid as option"
+    fi
 }
 
 func-parse() {
-	O_ALL=false
-	O_CONFIRM=false
-	O_EMPTY=false
-	O_LIST=false
+    O_ALL=false
+    O_CONFIRM=false
+    O_EMPTY=false
+    O_LIST=false
 
-	local PATTERN_SHORT="acel"
-	local PATTERN_LONG="all|confirm|empty|list"
+    readonly PATTERN_SHORT="acel"
+    readonly PATTERN_LONG="all|confirm|empty|list"
 
-	while (($# > 0)); do
-		case "$1" in
-			-h | --help)
-				func-usage
-				;;
-			-v | --version)
-				func-version
-				;;
-			-*)
-				func-validation "$1"
+    while (($# > 0)); do
+        case "$1" in
+        -h | --help)
+            printf "\nUsage: %s [options] [files or directories]\n" "$NAME"
+            printf "\nOptions:\n"
+            printf "  -a, --all     Show all items in Trash including hidden.\n"
+            printf "  -e, --empty   Permanently delete items in Trash.\n"
+            printf "  -c, --confirm Prompt before remove items in Trash.\n"
+            printf "  -l, --list    Show items in the Trash in long format.\n"
+            printf "  -h, --help    Display this help and exit.\n"
+            printf "  -v, --version Output version information and exit.\n"
+            exit 0
+            ;;
+        -v | --version)
+            echo "$NAME: $VERSION"
+            exit 0
+            ;;
+        -*)
+            func-validation "$1"
 
-				if [[ "$1" =~ ^(-[^-]*a|--all$) ]]; then O_ALL=true; fi
-				if [[ "$1" =~ ^(-[^-]*c|--confirm$) ]]; then O_CONFIRM=true; fi
-				if [[ "$1" =~ ^(-[^-]*e|--empty$) ]]; then O_EMPTY=true; fi
-				if [[ "$1" =~ ^(-[^-]*l|--list$) ]]; then O_LIST=true; fi
+            if [[ "$1" =~ ^(-[^-]*a|--all$) ]]; then O_ALL=true; fi
+            if [[ "$1" =~ ^(-[^-]*c|--confirm$) ]]; then O_CONFIRM=true; fi
+            if [[ "$1" =~ ^(-[^-]*e|--empty$) ]]; then O_EMPTY=true; fi
+            if [[ "$1" =~ ^(-[^-]*l|--list$) ]]; then O_LIST=true; fi
 
-				shift
-				;;
-			*)
-				((++argc))
-				argv+=("$1")
-				shift
-				;;
-		esac
-	done
+            shift
+            ;;
+        *)
+            ((++argc))
+            argv+=("$1")
+            shift
+            ;;
+        esac
+    done
 }
 
 func-list() {
-	local c='ls --color=auto'
-	"${O_ALL}" && c="$c -A"
-	"${O_LIST}" && c="$c -l"
-	$c "$TRASH_FILES"
+    local c='ls'
+    "${O_ALL}" && c="$c -A"
+    "${O_LIST}" && c="$c -l"
+    command $c --color=auto "$TRASH_FILES"
 }
 
-func-delete() {
-	if "${O_CONFIRM}"; then
-		read -r -p "total $(ls -A -U1 "$TRASH_FILES" | wc -l), Empties Trash? (y/N): " INPUT
-		case "$INPUT" in
-			y | Y | yes | Yes) ;;
-			*) exit 0 ;;
-		esac
-	fi
+func-remove() {
+    if "${O_CONFIRM}"; then
+        read -r -p "Items $(ls -A -U1 "$TRASH_FILES" | wc -l), Empty the trash? (y/N): " INPUT
+        case "$INPUT" in
+        y | Y | yes | Yes) ;;
+        *) exit 0 ;;
+        esac
+    fi
 
-	while IFS= read -r -d '' v; do
-		sh -c "rm -rf '$v'"
-		[ ! -d "$v" ] && mkdir "$v" && chown "$USERNAME:$USERNAME" "$v"
-	done < <(find "${TRASH_ROOT:?}/"* -maxdepth 0 -type d -print0)
+    local v
+    while IFS= read -r -d '' v; do
+        local err
+        if ! err="$(rm -rf "$v" 2>&1 >/dev/null)"; then
+            if [[ "$err" == *'Permission denied'* ]]; then
+                sudo rm -rf "$v"
+            else
+                echo "$err"
+            fi
+        fi
+        [ ! -d "$v" ] && mkdir "$v"
+    done < <(find "${TRASH_ROOT:?}/"* -maxdepth 0 -type d -print0)
 }
 
 func-move() {
-	for v in "${argv[@]}"; do
-		sh -c "mv -f '$v' $TRASH_FILES"
-	done
+    for v in "${argv[@]}"; do
+        # Trim trailing slash
+        v=${v%/}
+
+        # Basename
+        local name=${v##*/}
+
+        # Avoid the same name.
+        local suffix=''
+        local -i i=1
+        while [ -e "${TRASH_FILES}/${name}${suffix}" ]; do
+            suffix=".${i}"
+            ((i++))
+        done
+
+        local err
+        if ! err="$(mv -f "$v" "${TRASH_FILES}/${name}${suffix}" 2>&1 >/dev/null)"; then
+            if [[ "$err" == *'Permission denied'* ]]; then
+                sudo mv -f "$v" "${TRASH_FILES}/${name}${suffix}"
+            else
+                echo "$err"
+            fi
+        fi
+    done
 }
 
 func-main() {
-	NAME=$(basename "$0")
-	readonly NAME
+    local -i argc=0
+    local -a argv=()
 
-	VERSION="v0.0.3"
-	readonly VERSION
+    func-parse "$@"
 
-	local -i argc=0
-	local -a argv=()
-
-	func-parse "$@"
-
-	local USERNAME="$USER"
-	# logname command does not change the user name
-	# to root even if elevated privileges.
-	# So use this instead of whoami, $HOME or $USER.
-	if type logname > /dev/null 2>&1; then
-		USERNAME="$(logname 2> /dev/null || echo "$USERNAME")"
-	fi
-	TRASH_ROOT="/home/$USERNAME/.local/share/Trash"
-	readonly TRASH_ROOT
-
-	TRASH_FILES="$TRASH_ROOT/files"
-	readonly TRASH_FILES
-
-	if [ "$argc" -gt 0 ]; then
-		func-move
-		exit "$?"
-	fi
-
-	"${O_LIST}" || "${O_ALL}" && func-list
-	"${O_EMPTY}" || "${O_CONFIRM}" && func-delete
+    if [ "$argc" -gt 0 ]; then
+        func-move
+        exit $?
+    fi
+    if "${O_LIST}" || "${O_ALL}"; then
+        func-list
+    fi
+    if "${O_EMPTY}" || "${O_CONFIRM}"; then
+        func-remove
+    fi
+    exit $?
 }
 
 func-main "$@"
